5bd6cb665b6aba365704157831af520b8964cb49
Stanciu Gabriel <gabistanciu24@yahoo.com>
2023-10-19 18:00:27 +0300

    Make app deployable.

--- data/database.js
+++ data/database.js
@@ -1,11 +1,16 @@
 const mongodb = require("mongodb");
+const express = express();
+const PORT = process.env.PORT || 3000;
 
+const uri = process.env.MONGO_CONNECTION_STRING;
 const MongoClient = mongodb.MongoClient;
+
+const app = express();
 
 let database;
 
 async function connectToDatabase() {
-  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  const client = await MongoClient.connect(process.env.uri);
   database = client.db("online-shop");
 }
 
@@ -16,6 +21,16 @@ function getDb() {
 
   return database;
 }
+
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return false;
+  }
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
 
 module.exports = {
   connectToDatabase: connectToDatabase,
